ef8f326673984c9f6b3aa3dbcc83e0b57934970d
amelialloyd31@gmail.com <amelialloyd31@gmail.com>
2018-08-07 20:57:42 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(element,array) {
+  ["element",...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(element,array) {
+  array.unshift("element")
+}
+
+function accessElementInArray (array, index) {
+  return(array[index])
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array = array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array = array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0,array.lenth - 1)
+}
