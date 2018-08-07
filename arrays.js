0a75cd7fd503cf0d2ff3aa0f1b62b9ba7dab997c
amelialloyd31@gmail.com <amelialloyd31@gmail.com>
2018-08-07 21:15:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
+  return array = array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
